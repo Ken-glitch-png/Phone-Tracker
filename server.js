254fc997080776a6a04e5fbe12fc8a0e7697bb00
Ken-glitch-png <epikennethtaqueban@gmail.com>
2025-08-28 09:57:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1203,6 +1203,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Catch-all handler: send back React's index.html file for any non-API routes
+app.get('*', (req, res) => {
+  // Don't serve index.html for API routes
+  if (req.path.startsWith('/api/')) {
+    return res.status(404).json({ error: 'API endpoint not found' });
+  }
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
